ac14a398cc322dc161350aa804bf5e0c1784f928
Swatantra Sharma <105961629+swatantrasharma219@users.noreply.github.com>
2022-09-18 20:49:34 +0530

    Create find_values.java

---
+++ find_values.java
@@ -0,0 +1,37 @@
+public class values {
+public static void main(String[] args){
+  int x=2;
+  int y=5;
+  int z=0;
+  if(x==2){
+   System.out.println("true");
+  }
+  else{
+  System.out.println("false");
+  }
+  if(x!=5){
+  System.out.println("true");
+  }
+  else{
+  System.out.println("false");
+  }
+  if(x!=5 && y>=5){
+  System.out.println("true");
+  }
+  else{
+  System.out.println("false");
+  }
+  if(z!=0 || x==2){
+  System.out.println("true");  
+  }
+  else{
+  System.out.println("false");
+  }
+  if(!(y<10)){
+  System.out.println("true");
+  }
+  else{
+  System.out.println("false");
+  }
+    }
+}
